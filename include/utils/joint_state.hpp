44425f8f684873a724829e87be2d69b6eada8ca6
Tingjun Li <tingjunl@umich.edu>
2023-02-17 20:03:30 -0500

    Adjusted to Fetch's setting

--- include/utils/joint_state.hpp
+++ include/utils/joint_state.hpp
@@ -10,155 +10,168 @@
 #include <string>
 #include <memory>
 
-namespace husky_inekf {
+namespace husky_inekf
+{
 
-    class JointStateMeasurement : public Measurement {
-        public:
-            // Construct Encoder measurement
-            JointStateMeasurement(unsigned int ENCODER_DIM) {
-                type_ = JOINT_STATE;
-                encoder_dim_ = ENCODER_DIM;
+    class JointStateMeasurement : public Measurement
+    {
+    public:
+        // Construct Encoder measurement
+        JointStateMeasurement(unsigned int ENCODER_DIM)
+        {
+            type_ = JOINT_STATE;
+            encoder_dim_ = ENCODER_DIM;
+        }
+
+        // Constructor with default wheel radius
+        JointStateMeasurement(const sensor_msgs::JointState &joint_msg,
+                              unsigned int ENCODER_DIM) : encoder_dim_(ENCODER_DIM)
+        {
+
+            wheel_radius_ = 0.06033;
+            vehicle_track_width_ = 0.3748;
+            vehicle_length_ = 0.440;
+            type_ = JOINT_STATE;
+            joint_position_.resize(encoder_dim_, 1);
+            joint_velocity_.resize(encoder_dim_, 1);
+            joint_effort_.resize(encoder_dim_, 1);
+            linear_velocity_.resize(encoder_dim_, 1);
+            // TODO: Make this initialization more flexible if necessary
+            body_lin_vel_.resize(3, 1);
+            body_ang_vel_.resize(3, 1);
+            // Initialize Robot State
+            setJointPosition(joint_msg.position);
+            setJointVelocity(joint_msg.velocity);
+            setJointEffort(joint_msg.effort);
+            setLinearVelocity();
+            setBodyVelocity();
+
+            setHeader(joint_msg.header);
+        }
+
+        JointStateMeasurement(const sensor_msgs::JointState &joint_msg,
+                              unsigned int ENCODER_DIM,
+                              double wheel_radius_in,
+                              double vehicle_track_width_in,
+                              double vehicle_length_in) : encoder_dim_(ENCODER_DIM),
+                                                          wheel_radius_(wheel_radius_in),
+                                                          vehicle_track_width_(vehicle_track_width_in),
+                                                          vehicle_length_(vehicle_length_in)
+        {
+            type_ = JOINT_STATE;
+            joint_position_.resize(encoder_dim_, 1);
+            joint_velocity_.resize(encoder_dim_, 1);
+            joint_effort_.resize(encoder_dim_, 1);
+            linear_velocity_.resize(encoder_dim_, 1);
+            // TODO: Make this initialization more flexible if necessary
+            body_lin_vel_.resize(3, 1);
+            body_ang_vel_.resize(3, 1);
+            // Initialize Robot State
+            setJointPosition(joint_msg.position);
+            setJointVelocity(joint_msg.velocity);
+            setJointEffort(joint_msg.effort);
+            setLinearVelocity();
+            setBodyVelocity();
+
+            setHeader(joint_msg.header);
+        }
+
+        void setJointPosition(const std::vector<double> &position)
+        {
+            for (int i = 0; i < encoder_dim_; ++i)
+            {
+                joint_position_(i) = position[i];
             }
-            
-            // Constructor with default wheel radius
-            JointStateMeasurement(  const sensor_msgs::JointState& joint_msg,
-                                    unsigned int ENCODER_DIM): 
-                                    encoder_dim_(ENCODER_DIM){
+        }
 
-                wheel_radius_ = 0.1651;
-                vehicle_track_width_ = 0.555;
-                vehicle_length_ = 0.540;
-                type_ = JOINT_STATE;
-                joint_position_.resize(encoder_dim_, 1);
-                joint_velocity_.resize(encoder_dim_, 1);
-                joint_effort_.resize(encoder_dim_, 1);
-                linear_velocity_.resize(encoder_dim_, 1);
-                // TODO: Make this initialization more flexible if necessary
-                body_lin_vel_.resize(3, 1);
-                body_ang_vel_.resize(3, 1);
-                // Initialize Robot State
-                setJointPosition(joint_msg.position);
-                setJointVelocity(joint_msg.velocity);
-                setJointEffort(joint_msg.effort);
-                setLinearVelocity();
-                setBodyVelocity();
-
-                setHeader(joint_msg.header);
+        void setJointVelocity(const std::vector<double> &velocity)
+        {
+            for (int i = 0; i < encoder_dim_; ++i)
+            {
+                joint_velocity_(i) = velocity[i];
             }
+        }
 
-            JointStateMeasurement(  const sensor_msgs::JointState& joint_msg,
-                                    unsigned int ENCODER_DIM,
-                                    double wheel_radius_in,
-                                    double vehicle_track_width_in,
-                                    double vehicle_length_in): 
-                                    encoder_dim_(ENCODER_DIM),
-                                    wheel_radius_(wheel_radius_in),
-                                    vehicle_track_width_(vehicle_track_width_in),
-                                    vehicle_length_(vehicle_length_in){
-                type_ = JOINT_STATE;
-                joint_position_.resize(encoder_dim_, 1);
-                joint_velocity_.resize(encoder_dim_, 1);
-                joint_effort_.resize(encoder_dim_, 1);
-                linear_velocity_.resize(encoder_dim_, 1);
-                // TODO: Make this initialization more flexible if necessary
-                body_lin_vel_.resize(3, 1);
-                body_ang_vel_.resize(3, 1);
-                // Initialize Robot State
-                setJointPosition(joint_msg.position);
-                setJointVelocity(joint_msg.velocity);
-                setJointEffort(joint_msg.effort);
-                setLinearVelocity();
-                setBodyVelocity();
-
-                setHeader(joint_msg.header);
+        void setJointEffort(const std::vector<double> &effort)
+        {
+            for (int i = 0; i < encoder_dim_; ++i)
+            {
+                joint_effort_(i) = effort[i];
             }
+        }
 
-            void setJointPosition(const std::vector<double>& position) {
-                for (int i = 0; i < encoder_dim_; ++i) {
-                    joint_position_(i) = position[i];
-                }
+        void setLinearVelocity()
+        {
+            for (int i = 0; i < encoder_dim_; ++i)
+            {
+                linear_velocity_(i) = joint_velocity_(i) * wheel_radius_;
             }
+        }
 
-            void setJointVelocity(const std::vector<double>& velocity) {
-                for (int i = 0; i < encoder_dim_; ++i) {
-                    joint_velocity_(i) = velocity[i];
-                }
-            }
+        void setBodyVelocity()
+        {
+            body_lin_vel_.setZero();
+            body_ang_vel_.setZero();
 
-            void setJointEffort(const std::vector<double>& effort) {
-                for (int i = 0; i < encoder_dim_; ++i) {
-                    joint_effort_(i) = effort[i];
-                }
-            }
+            double vr = linear_velocity_(1);
+            double vl = linear_velocity_(0);
+            body_lin_vel_(0) = (vr + vl) / 2.0;                                                                                             // [x y z]
+            body_ang_vel_(2) = (vr - vl) / vehicle_track_width_ * std::pow(std::cos(std::atan2(vehicle_length_, vehicle_track_width_)), 2); // [dx dy dz]
+        }
 
-            void setLinearVelocity() {
-                for (int i = 0; i < encoder_dim_; ++i) {
-                    linear_velocity_(i) = joint_velocity_(i) * wheel_radius_; 
-                }
-            }
+        inline const Eigen::VectorXd &getJointPosition() const
+        {
+            return joint_position_;
+        }
 
-            void setBodyVelocity() {
-                body_lin_vel_.setZero();
-                body_ang_vel_.setZero();
+        inline const Eigen::VectorXd &getJointVelocity() const
+        {
+            return joint_velocity_;
+        }
 
-                double vr = (linear_velocity_(1) + linear_velocity_(3)) / 2.0;
-                double vl = (linear_velocity_(0) + linear_velocity_(2)) / 2.0;
-                body_lin_vel_(0) = (vr + vl) / 2.0 ; // [x y z]
-                body_ang_vel_(2) = (vr - vl) / vehicle_track_width_* std::pow(std::cos(std::atan2(vehicle_length_, vehicle_track_width_)), 2); // [dx dy dz]
-            }
+        inline const Eigen::VectorXd &getJointEffort() const
+        {
+            return joint_effort_;
+        }
 
-            inline const Eigen::VectorXd& getJointPosition() const {
-                return joint_position_;
-            }
+        inline const Eigen::VectorXd &getLinearVelocity() const
+        {
+            return linear_velocity_;
+        }
 
-            inline const Eigen::VectorXd& getJointVelocity() const {
-                return joint_velocity_;
-            }
+        inline const Eigen::VectorXd &getBodyLinearVelocity() const
+        {
+            return body_lin_vel_;
+        }
 
-            inline const Eigen::VectorXd& getJointEffort() const {
-                return joint_effort_;
-            }
+        inline const Eigen::VectorXd &getBodyAngularVelocity() const
+        {
+            return body_ang_vel_;
+        }
 
-            inline const Eigen::VectorXd& getLinearVelocity() const {
-                return linear_velocity_;
-            }
+    private:
+        double wheel_radius_;
+        // static constexpr double wheel_radius_ = 0.3;
 
-            inline const Eigen::VectorXd&  getBodyLinearVelocity() const {
-                return body_lin_vel_;
-            }
+        double vehicle_track_width_;
+        double vehicle_length_;
+        unsigned int encoder_dim_;
 
-            inline const Eigen::VectorXd&  getBodyAngularVelocity() const {
-                return body_ang_vel_;
-            }
-    
-        private:
-            double wheel_radius_;
-            // static constexpr double wheel_radius_ = 0.3;
+        // Joint Encoder Information From Wheels
+        //  [front_left_wheel, front_right_wheel, rear_left_wheel, rear_right_wheel]
+        // https://github.com/husky/husky/issues/172
+        Eigen::VectorXd joint_position_;
+        Eigen::VectorXd joint_velocity_;
+        Eigen::VectorXd joint_effort_;
 
-            double vehicle_track_width_;
-            double vehicle_length_;
-            unsigned int encoder_dim_; 
+        Eigen::VectorXd linear_velocity_;
 
-            // Joint Encoder Information From Wheels
-            //  [front_left_wheel, front_right_wheel, rear_left_wheel, rear_right_wheel]
-            // https://github.com/husky/husky/issues/172
-            Eigen::VectorXd joint_position_;
-            Eigen::VectorXd joint_velocity_;
-            Eigen::VectorXd joint_effort_;
-
-            // linear wheel velocity
-            // linear_velocity = joint_velocity * wheel_radius
-            // wheel_radius = 0.1651 m
-            // https://github.com/husky/husky/blob/noetic-devel/husky_description/urdf/husky.urdf.xacro#L79
-
-            Eigen::VectorXd linear_velocity_;
-            
-            // body velocity from wheel encoders
-            // body_lin_vel = (v_r + v_l) / 2
-            // body_ang_vel = (v_r-v_l)/w
-            // w = 0.555 m
-            Eigen::VectorXd body_lin_vel_;
-            Eigen::VectorXd body_ang_vel_;
+        // body velocity from wheel encoders
+        // body_lin_vel = (v_r + v_l) / 2
+        // body_ang_vel = (v_r-v_l)/w
+        // w = 0.555 m
+        Eigen::VectorXd body_lin_vel_;
+        Eigen::VectorXd body_ang_vel_;
     };
 
     typedef std::shared_ptr<JointStateMeasurement> JointStateMeasurementPtr;
--- src/communication/husky_comms.cpp
+++ src/communication/husky_comms.cpp
@@ -1,19 +1,18 @@
 #include "communication/husky_comms.h"
 
-
 #include <boost/timer/timer.hpp>
 
-HuskyComms::HuskyComms( ros::NodeHandle* nh, husky_inekf::husky_data_t* husky_data_buffer)
-                        : nh_(nh), husky_data_buffer_(husky_data_buffer)
+HuskyComms::HuskyComms(ros::NodeHandle *nh, husky_inekf::husky_data_t *husky_data_buffer)
+    : nh_(nh), husky_data_buffer_(husky_data_buffer)
 {
     std::string imu_topic, joint_topic, wheel_vel_topic, camera_vel_topic, gps_vel_topic;
-    
+
     nh_->param<std::string>("/settings/imu_topic", imu_topic, "/gx5_0/imu/data");
     nh_->param<std::string>("/settings/joint_topic", joint_topic, "/joint_states");
 
-    nh_->param<double>("/settings/wheel_radius",wheel_radius_,0.1651);
-    nh_->param<double>("/settings/vehicle_track_width",vehicle_track_width_,0.555);
-    nh_->param<double>("/settings/vehicle_length",vehicle_length_,0.540);
+    nh_->param<double>("/settings/wheel_radius", wheel_radius_, 0.06033);
+    nh_->param<double>("/settings/vehicle_track_width", vehicle_track_width_, 0.3748);
+    nh_->param<double>("/settings/vehicle_length", vehicle_length_, 0.440);
 
     // Velocity Type:
     nh_->param<bool>("/settings/enable_wheel_velocity_update", enable_wheel_vel_, true);
@@ -34,41 +33,46 @@ HuskyComms::HuskyComms( ros::NodeHandle* nh, husky_inekf::husky_data_t* husky_da
     nh_->param<std::vector<double>>("settings/rotation_cam_body", rotation_cam_body, std::vector<double>({1, 0, 0, 0}));
 
     Eigen::Quaternion<double> orientation_quat(rotation_cam_body[0],
-                                                rotation_cam_body[1],
-                                                rotation_cam_body[2],
-                                                rotation_cam_body[3]);
-    
-    cam_to_body_.block<3,3>(0,0) = orientation_quat.toRotationMatrix();
-    cam_to_body_.block<3,1>(0,3) = Eigen::Vector3d({translation_cam_body[0], translation_cam_body[1], translation_cam_body[2]});
-    
+                                               rotation_cam_body[1],
+                                               rotation_cam_body[2],
+                                               rotation_cam_body[3]);
+
+    cam_to_body_.block<3, 3>(0, 0) = orientation_quat.toRotationMatrix();
+    cam_to_body_.block<3, 1>(0, 3) = Eigen::Vector3d({translation_cam_body[0], translation_cam_body[1], translation_cam_body[2]});
+
     // Rotation from IMU to body frame. Body frame definition: X forward, Y left, Z up.
     nh_->param<std::vector<double>>("settings/rotation_imu_body", rotation_imu_body_, std::vector<double>({0, 0.7071, -0.7071, 0}));
 
-    std::cout<<"subscribing to: "<< imu_topic << joint_topic<<std::endl;
+    std::cout << "subscribing to: " << imu_topic << joint_topic << std::endl;
 
     // Initialize subscribers with queue size of 1000
     imu_sub_ = nh_->subscribe(imu_topic, 1000, &HuskyComms::imuCallback, this);
 
     // velocity from wheel encoders. msg_type: sensor_msgs::JointState
-    if(enable_wheel_vel_){
+    if (enable_wheel_vel_)
+    {
         wheel_vel_sub_ = nh_->subscribe(wheel_vel_topic, 1000, &HuskyComms::jointStateVelocityCallback, this);
     }
-    else{
+    else
+    {
         joint_sub_ = nh_->subscribe(joint_topic, 1000, &HuskyComms::jointStateCallback, this);
     }
 
     // velocity from camera odometry. msg_type: nav_msgs::Odometry
-    if(enable_camera_vel_){
+    if (enable_camera_vel_)
+    {
         cam_vel_sub_ = nh_->subscribe(camera_vel_topic, 1000, &HuskyComms::CameraOdomCallBack, this);
     }
 
     // velocity from GPS. msg_type: geometry_msgs::TwistStamped
-    if(enable_gps_vel_){
+    if (enable_gps_vel_)
+    {
         gps_vel_sub_ = nh_->subscribe(gps_vel_topic, 1000, &HuskyComms::GPSvelocityCallback, this);
     }
 
     // start the subscribing thread
-    subscribing_thread_ = std::thread([this]{this->sub();});
+    subscribing_thread_ = std::thread([this]
+                                      { this->sub(); });
 }
 
 HuskyComms::~HuskyComms()
@@ -76,37 +80,47 @@ HuskyComms::~HuskyComms()
     subscribing_thread_.join();
 }
 
-
-void HuskyComms::sub(){
-    while(ros::ok()){
+void HuskyComms::sub()
+{
+    while (ros::ok())
+    {
         ros::spinOnce();
     }
 }
 
 // Note no transformation mat needed between imu and odometry for husky
-void HuskyComms::imuCallback(const sensor_msgs::Imu& imu_msg) 
+void HuskyComms::imuCallback(const sensor_msgs::Imu &imu_msg)
 {
-    auto imu_ptr = 
-        std::make_shared<husky_inekf::ImuMeasurement<double> >(imu_msg, rotation_imu_body_);
+    auto imu_ptr =
+        std::make_shared<husky_inekf::ImuMeasurement<double>>(imu_msg, rotation_imu_body_);
 
     std::lock_guard<std::mutex> lock(husky_data_buffer_->imu_mutex);
     husky_data_buffer_->imu_q.push(imu_ptr);
 }
 
-
-void HuskyComms::jointStateCallback(const sensor_msgs::JointState& joint_msg)
-{   
+void HuskyComms::jointStateCallback(const sensor_msgs::JointState &joint_msg)
+{
+    if (joint_msg.velocity.size() <= 2)
+    {
+        // velocity message from wheel encoder is in an array of size greater than 2
+        return;
+    }
     auto joint_ptr =
-        std::make_shared<husky_inekf::JointStateMeasurement>(joint_msg, 4, wheel_radius_, vehicle_track_width_, vehicle_length_);
+        std::make_shared<husky_inekf::JointStateMeasurement>(joint_msg, 2, wheel_radius_, vehicle_track_width_, vehicle_length_);
 
     std::lock_guard<std::mutex> lock(husky_data_buffer_->joint_state_mutex);
     husky_data_buffer_->joint_state_q.push(joint_ptr);
 }
 
+void HuskyComms::jointStateVelocityCallback(const sensor_msgs::JointState &joint_msg)
+{
+    if (joint_msg.velocity.size() <= 2)
+    {
+        // velocity message from wheel encoder is in an array of size greater than 2
+        return;
+    }
 
-void HuskyComms::jointStateVelocityCallback(const sensor_msgs::JointState& joint_msg)
-{   
-    auto joint_state_ptr = std::make_shared<husky_inekf::JointStateMeasurement>(joint_msg, 4, wheel_radius_, vehicle_track_width_, vehicle_length_);
+    auto joint_state_ptr = std::make_shared<husky_inekf::JointStateMeasurement>(joint_msg, 2, wheel_radius_, vehicle_track_width_, vehicle_length_);
 
     // set velocity message:
     geometry_msgs::TwistStamped vel_msg;
@@ -121,39 +135,39 @@ void HuskyComms::jointStateVelocityCallback(const sensor_msgs::JointState& joint
     vel_msg.twist.angular.z = joint_state_ptr->getBodyAngularVelocity()(2);
     auto vel_ptr = std::make_shared<husky_inekf::VelocityMeasurement>(vel_msg);
 
-
     std::lock_guard<std::mutex> lock2(husky_data_buffer_->joint_state_mutex);
     husky_data_buffer_->joint_state_q.push(joint_state_ptr);
 
     std::lock_guard<std::mutex> lock(husky_data_buffer_->wheel_vel_mutex);
-    husky_data_buffer_ -> wheel_velocity_q.push(vel_ptr);
-        
+    husky_data_buffer_->wheel_velocity_q.push(vel_ptr);
 }
 
-
-void HuskyComms::GPSvelocityCallback(const geometry_msgs::TwistStamped& vel_msg){
+void HuskyComms::GPSvelocityCallback(const geometry_msgs::TwistStamped &vel_msg)
+{
     auto vel_ptr = std::make_shared<husky_inekf::VelocityMeasurement>(vel_msg);
     std::lock_guard<std::mutex> lock(husky_data_buffer_->gps_vel_mutex);
-    husky_data_buffer_ -> gps_velocity_q.push(vel_ptr);
+    husky_data_buffer_->gps_velocity_q.push(vel_ptr);
 }
 
-void HuskyComms::CameraOdomCallBack(const nav_msgs::Odometry& camera_odom_msg) {
+void HuskyComms::CameraOdomCallBack(const nav_msgs::Odometry &camera_odom_msg)
+{
     auto camera_odom_ptr = std::make_shared<husky_inekf::CameraOdomMeasurement>(camera_odom_msg);
-    
+
     // We need two odometry data to calculate the velocity
-    if (husky_data_buffer_ -> camera_odom_q.empty()) {
-        husky_data_buffer_ -> camera_odom_q.push(camera_odom_ptr);
+    if (husky_data_buffer_->camera_odom_q.empty())
+    {
+        husky_data_buffer_->camera_odom_q.push(camera_odom_ptr);
         return;
     }
 
-    auto prev_transformation = 
-            husky_data_buffer_->camera_odom_q.front().get()->getTransformation();
+    auto prev_transformation =
+        husky_data_buffer_->camera_odom_q.front().get()->getTransformation();
     double prev_time = husky_data_buffer_->camera_odom_q.front().get()->getTime();
 
     husky_data_buffer_->camera_odom_q.pop();
 
     // Insert the current camera odometry:
-    husky_data_buffer_ -> camera_odom_q.push(camera_odom_ptr);
+    husky_data_buffer_->camera_odom_q.push(camera_odom_ptr);
     auto curr_transformation = camera_odom_ptr->getTransformation();
     double curr_time = camera_odom_ptr->getTime();
 
@@ -173,9 +187,9 @@ void HuskyComms::CameraOdomCallBack(const nav_msgs::Odometry& camera_odom_msg) {
     vel_msg.twist.angular.x = twist_se3(2, 1) / time_diff;
     vel_msg.twist.angular.y = -twist_se3(2, 0) / time_diff;
     vel_msg.twist.angular.z = twist_se3(1, 0) / time_diff;
-    
+
     auto vel_ptr = std::make_shared<husky_inekf::VelocityMeasurement>(vel_msg);
 
     std::lock_guard<std::mutex> lock(husky_data_buffer_->cam_vel_mutex);
-    husky_data_buffer_ -> camera_velocity_q.push(vel_ptr);
+    husky_data_buffer_->camera_velocity_q.push(vel_ptr);
 }
